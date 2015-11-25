b634cb0944acf96cd0e5bbc8d3be6193c217b027
mbrownzopa <martin.brown@zopa.com>
2015-11-25 17:12:41 +0000

    New bash scripts

---
+++ Underwriting/Policy/Sandbox/job.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd $(dirname $0)
+python /home/administrator/test_tools/Underwriting/Policy/Sandbox/checkCodes.py
---
+++ Underwriting/Segment/Prod/job.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd $(dirname $0)
+python /home/administrator/test_tools/Underwriting/Segment/Prod/checkCodes.py
